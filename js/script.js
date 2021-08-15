f08a0c06cce0d9a6822b372a69c588a341c957c1
vildehys <79218980+vildehys@users.noreply.github.com>
2021-08-15 19:20:43 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// Question 1 
+// Create two number variables, one with integer value and one with a decimal value
+
+var number1 = 6
+var number2 = 3.9
+
+
+// Question 2
+// Declare and intialise a variable with all the working days of the week seperated by hyphens
+
+
+
+// Question 3
+// Create four variables that demonstrate the following operations:
+
+// - addition
+// - subtraction
+// - multiplication
+// - division
+
+
+// Question 4
+//Declare a boolean variable with an intial value of true or false
+
+
+//Question 5
+//Console log the number of characters in this string: "The frogs are angry."
+
+
+//Question 6
+//Convet the following variable ames to camelCase
+
+var time_elapsed;
+var totalprice;
+
+
+// Question 7
+// Convert the following stringvalues to number values:
+
+var number1 = "53";
+var number2 = "44,6";
+
+
+// Question 8 
+// Check the type of the following variables and log the results:
+
+var pet = "donkey";
+var age = "89";
+var discountApplied = true;
+var timeLeft;
+
+
+// Question 9
+//Declare and initialise a variable with the value "updating heading".
+//Select the h1 element on the page and set it's inner HTML value to be the variable created above
+
+
+// Question 10
+// Select the body element on the page and change its background colour to loghtskyblue
